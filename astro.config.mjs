95c3c347846d5eeba2c48cf1b4006089ce1789a3
suzu-shika <msz.006.ts@gmail.com>
2026-06-27 14:53:49 +0900

    2026-06-27_sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-// import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
 
-// https://astro.build/config
 export default defineConfig({
-  // site: "https://oitacampuslink.com/",
-  trailingSlash: "never",
-  // integrations: [sitemap()],
+  site: 'https://oitacampuslink.com',
+  integrations: [sitemap()]
 });
